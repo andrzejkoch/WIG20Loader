13229c97a50b52dd92a07ba6f7479bd831f215be
Andrzej Koch <andrzejkoch@poczta.onet.pl>
2019-03-19 10:21:09 +0100

    First commit

---
+++ src/WIG20loader.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WIG20loader {
+    public static void main(String[] args) {
+        List<String> sharePriceList = new ArrayList<>();
+
+        File file = new File("C:\\Users\\rozpet\\Documents\\java\\Projects\\WIG20loader\\data\\ALIOR.prn");
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String rowFromFile = null;
+
+            while ((rowFromFile = reader.readLine()) != null) {
+                String[] parts = rowFromFile.split(",");
+                String shareName = parts[0];
+                String shareDate = parts[2];
+                String shareHour = parts[3];
+                String sharePrice = parts[4];
+                if (Integer.parseInt(shareDate)>=20180101){
+                    System.out.println(shareDate+" "+shareHour+" "+sharePrice);
+                    sharePriceList.add(rowFromFile);
+
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+//        System.out.println(sharePriceList);
+    }
+
+}
