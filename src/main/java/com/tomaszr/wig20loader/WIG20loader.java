415fca9c232e3d1835ecdc5016995d29ca460888
Andrzej Koch <andrzejkoch@poczta.onet.pl>
2019-03-27 14:58:25 +0100

    while loop added to load data rows from the file

--- src/main/java/com/tomaszr/wig20loader/WIG20loader.java
+++ src/main/java/com/tomaszr/wig20loader/WIG20loader.java
@@ -10,9 +10,13 @@ import java.util.List;
 
 public class WIG20loader {
     public static void main(String[] args) {
-        List<String> sharePriceList = new ArrayList<>();
+//        List<String> sharePriceList = new ArrayList<>();
 
-        File file = new File("C:\\Users\\rozpet\\Documents\\java\\Projects\\WIG20Loader\\src\\main\\resources\\data\\ALIOR.prn");
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//        Wig20Entity wig20 = new Wig20Entity();
+
+        File file = new File("C:\\Users\\rozpet\\Documents\\java\\Projects\\WIG20Loader\\src\\main\\resources\\data\\PGNIG.prn");
         BufferedReader reader = null;
 
         try {
@@ -25,9 +29,25 @@ public class WIG20loader {
                 String shareDate = parts[2];
                 String shareHour = parts[3];
                 String sharePrice = parts[4];
-                if (Integer.parseInt(shareDate)>=20180101){
-//                    System.out.println(shareDate+" "+shareHour+" "+sharePrice);
-                    sharePriceList.add(rowFromFile);
+                if (Integer.parseInt(shareDate) >= 20180101) {
+                    System.out.println(shareDate + " " + shareHour + " " + sharePrice);
+                    Session session = HibernateUtil.getSessionFactory().openSession();
+                    session.beginTransaction();
+                    Wig20Entity wig20 = new Wig20Entity();
+
+//                    sharePriceList.add(rowFromFile);
+                    wig20.setName(shareName);
+                    wig20.setDate(shareDate);
+                    wig20.setHour(shareHour);
+                    wig20.setPrice(Double.parseDouble(sharePrice));
+
+                    //Save the employee in database
+                    session.save(wig20);
+
+                    //Commit the transaction
+                    session.getTransaction().commit();
+                    HibernateUtil.shutdown();
+
 
                 }
 
@@ -45,22 +65,21 @@ public class WIG20loader {
             }
         }
 //        System.out.println(sharePriceList);
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//
+//        Wig20Entity wig20 = new Wig20Entity();
 
-        Wig20Entity wig20 = new Wig20Entity();
-
-//        wig20.setId(2L);
-        wig20.setName("ALIOR");
-        wig20.setDate("20190312");
-        wig20.setHour("164951");
-        wig20.setPrice(12.14);
+//        wig20.setId(9L);
+//        wig20.setName("ALIOR");
+//        wig20.setDate("20190312");
+//        wig20.setHour("164951");
+//        wig20.setPrice(12.11);
 
         //Save the employee in database
-        session.save(wig20);
+//        session.save(wig20);
+//        session.getTransaction().commit();
 
-        //Commit the transaction
-        session.getTransaction().commit();
         HibernateUtil.shutdown();
 
     }
